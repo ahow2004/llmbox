b144ec1ce73b9d0ab2b024ad90c97f7a334dbbea
ahow2004 <austin.howard2024@gmail.com>
2025-04-24 14:15:28 -0600

    Update +layout.svelte

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,5 @@
 <script>
-  import { ClerkProvider } from '@clerk/clerk-js';
   import { browser } from '$app/environment';
-
-  const publishableKey = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY;
 </script>
 
-{#if browser}
-  <ClerkProvider publishableKey={publishableKey}>
-    <slot />
-  </ClerkProvider>
-{:else}
-  <slot />
-{/if}
+<slot />
